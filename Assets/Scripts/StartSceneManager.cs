405251612587a0641cc264a48a6c8606731e26b8
Haroldlo0320 <Chung.H.Lo@student.uts.edu.au>
2024-11-01 14:13:04 +0800

    the mainscene'

---
+++ Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartSceneManager : MonoBehaviour
+{
+    public Button levelOneButton;
+    public Button levelTwoButton;
+    public Text highScoreText;
+    public Text timeText;
+    public AudioSource backgroundMusic;
+
+    void Start()
+    {
+        // Assign button listeners
+        if (levelOneButton != null)
+            levelOneButton.onClick.AddListener(LoadLevel1);
+
+        if (levelTwoButton != null)
+            levelTwoButton.onClick.AddListener(LoadLevel2);
+
+        // Initialize high score and time
+        highScoreText.text = "High Score: 0";
+        timeText.text = "Time: 00:00:00";
+
+        // Play background music
+        if (backgroundMusic != null)
+        {
+            backgroundMusic.loop = true;
+            backgroundMusic.Play();
+        }
+    }
+
+    void LoadLevel1()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    void LoadLevel2()
+    {
+        // Replace "DesignIterationScene" with your actual scene name
+        if (Application.CanStreamedLevelBeLoaded("DesignIterationScene"))
+        {
+            SceneManager.LoadScene("DesignIterationScene");
+        }
+        else
+        {
+            Debug.Log("Design Iteration Scene not available.");
+            // Optionally, display a message to the player
+        }
+    }
+}
